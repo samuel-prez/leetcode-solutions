8658e613efb937d6d6cedab09b2d0a9f6d204f34
Samuel Pérez <zetrax190@gmail.com>
2025-05-19 23:39:50 -0500

    tokens code improved

--- easy/Java String Tokens.java
+++ easy/Java String Tokens.java
@@ -28,9 +28,14 @@ public class First_Solution {
 public class Solution_opti {
 
     public static void main(String[] args) {
-         Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
         // Write your code here.
+        s = s.trim();
+        if (s.isEmpty()) {
+            System.out.println(0);
+            return;
+        }
         String[] tokens = s.split("[ !,?._'@]+");
         System.out.println(tokens.length);
         for(String tok : tokens){
