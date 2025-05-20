69dfca75da69d2b9239f3638ee2e536f655efa17
Samuel Pérez <zetrax190@gmail.com>
2025-05-19 23:30:15 -0500

    java string tokens exercise

---
+++ easy/Java String Tokens.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class First_Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        String[] tokens = s.split("[ !,?._'@]+");
+        String resultado = "";
+        int contTokens = 0;
+        for(int i=0;i<tokens.length;i++){
+            contTokens++;
+            resultado += tokens[i];
+            if(i<tokens.length-1){
+                resultado += "\n";
+            }
+        }
+        System.out.println(contTokens);
+        System.out.println(resultado);
+        scan.close();
+    }
+}
+
+
+
+public class Solution_opti {
+
+    public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        String[] tokens = s.split("[ !,?._'@]+");
+        System.out.println(tokens.length);
+        for(String tok : tokens){
+           System.out.println(tok); 
+        }
+        scan.close();
+    }
+}
+
